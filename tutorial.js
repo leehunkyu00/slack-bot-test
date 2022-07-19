24cf997b72579af47b6e3e132640cd4c15aba67e
hklee <leehunkyu00@gmail.com>
2022-07-19 14:11:36 +0900

    send slack for message hello

--- tutorial.js
+++ tutorial.js
@@ -1,3 +1,29 @@
 const { WebClient } = require('@slack/web-api');
+require("dotenv").config();
 
+
+console.log("SLACK_TOKEN :", process.env.SLACK_TOKEN);
 console.log('Getting started with Node Slack SDK');
+
+if (process.env.SLACK_TOKEN == undefined) {
+  console.log("Need SLACK_TOKEN in .env");
+  process.exit(9);
+}
+// The current webclient
+const web = new WebClient(process.env.SLACK_TOKEN);
+// The current date
+const currentTime = new Date().toTimeString();
+
+(async () => {
+  try {
+    // Use the `chat.postMessage` method to send a message from this app
+    await web.chat.postMessage({
+      channel: '#일반',
+      text: `The current time is ${currentTime}`,
+    });
+    console.log('Message posted!');
+  } catch (error) {
+    console.log(error);
+  }
+
+})();
